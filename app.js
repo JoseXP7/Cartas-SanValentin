5475a1da23094446b39cf7f5db5d90bb8afbd275
JoseXP7 <josemanuelgraterolrodriguez@gmail.com>
2024-02-04 02:52:02 -0400

    Cambios paleta de colores bootstrap, pagina de thanks terminada, y cambios del index html

--- app.js
+++ app.js
@@ -1,38 +1,62 @@
-let cuerpocaja = document.getElementById("cuerposection")
-let confeti = document.querySelector("#my-canvas")
+let formulario = document.getElementById('formulario')
 
-let nombre = document.getElementById("nombre-user").value
-let correo = document.getElementById("email").value
-let para = document.getElementById("receptor").value
-let cartita = document.getElementById("carta").value
-let enviar = document.getElementById("buttonsend")
+formulario.addEventListener("submit", validacionForm);
 
 Swal.fire({
-        imageUrl: "assets/mileslogo.png",
+        imageUrl: "assets/cedcytvertical.png",
         imageWidth: 135,
-      imageHeight: 102,
+      imageHeight: 93,
         title: "Instrucciones:",
-        text: "Rellena los campos correctamente, asegurate que tu correo este activo y sin errores. No te olvides de indicar en las casillas de verificacion si tu carta quieres que sea anonima o quieres que la persona a recibir vea su remitente",
+        text: "Rellena los campos correctamente, asegúrate que tu correo este activo y sin errores. No te olvides de indicar en las casillas de verificación si tu carta quieres que sea anónima o quieres que la persona a recibir vea su remitente",
         confirmButtonText: "OK!"
       });
 
-let navegador = navigator.userAgent;
+/*let navegador = navigator.userAgent;
         if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
             console.log("Estás usando un dispositivo móvil!!");
             document.getElementById("cuerposection").style.width = 100 + "%"
         } else {
             console.log("No estás usando un móvil");
+}*/
+
+function validacionForm(e) {
+
+  const nombre = document.getElementById('nombre').value
+  const email = document.getElementById('email').value
+  const receptor = document.getElementById('receptor').value
+
+  let emailRegEx = /[^@ \t\r\n]+@gmail\.com/g
+
+  if (nombre == '' && email == '' && receptor == '') {
+    e.preventDefault()
+    mensajeAlerta()
+  }
+
+  if (emailRegEx.test(email) == false) {
+    e.preventDefault()
+    mensajeEmail()
+  }
 }
 
-function validacionForm() {
-  if (document.getElementById("carta").value.length == 0) {
-  console.log("gei")
-} else {
-  confeti.classList.add("active")
-  } 
+function mensajeAlerta() {
+  Swal.fire({
+    icon: "error",
+    title: "¡Espacios en Blanco!",
+    text: "Por favor no dejes espacios en blanco, verifica",
+    confirmButtonText: "OK!"
+  })
 }
 
-function recaptcha_ok() {
+function mensajeEmail() {
+  Swal.fire({
+    icon: "error",
+    title: "¡Correo Inválido!",
+    text: "Por favor, verifica tu correo, solo aceptamos Gmail",
+    confirmButtonText: "OK!"
+  })
+}
+
+/*function recaptcha_ok() {
     let response = grecaptcha.getResponse();
 
     if(response.length == 0){
@@ -49,4 +73,4 @@ function recaptcha_ok() {
       });
       document.getElementById("buttonsend").disabled = false
     }
-  }
+  }*/
